882ee836965070acffde600e7b9131b6ec73e9bb
IAmNotABear <floyd.van.boksel@gmail.com>
2022-11-24 13:15:40 +0100

    Add support to possible thread count calculation to pass the used ram by server

--- src/scripts/utils/getPossibleThreadCount.ts
+++ src/scripts/utils/getPossibleThreadCount.ts
@@ -1,7 +1,16 @@
-export default (ns: NS, server: string, script: string): number => {
+export default (
+  ns: NS,
+  server: string,
+  script: string,
+  serverUsedRam?: number
+): number => {
   const serverMaxRam = ns.getServerMaxRam(server);
-  const serverUsedRam = ns.getServerUsedRam(server);
   const scriptRAM = ns.getScriptRam(script, server);
 
-  return Math.ceil(Math.floor((serverMaxRam - serverUsedRam) / scriptRAM));
+  return Math.ceil(
+    Math.floor(
+      (serverMaxRam - (serverUsedRam || ns.getServerUsedRam(server))) /
+        scriptRAM
+    )
+  );
 };
