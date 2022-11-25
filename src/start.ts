1c93d29a5841796c475956d3b87d5a4c0359516d
IAmNotABear <floyd.van.boksel@gmail.com>
2022-11-25 11:29:44 +0100

    Remove comments from startup script for now since most are redundant

--- src/start.ts
+++ src/start.ts
@@ -6,9 +6,7 @@ import {
 
 export async function main(ns: NS): Promise<void> {
   ns.tprint("Start");
-  /**
-   * Start the auto hacking
-   */
+
   if (!ns.isRunning("/scripts/core/server-auto-hack.js", "home")) {
     await ns.run("/scripts/core/server-auto-hack.js");
   }
@@ -17,35 +15,22 @@ export async function main(ns: NS): Promise<void> {
     await ns.run("/scripts/auto-buy/auto-buy.js");
   }
 
-  /**
-   * Start all servers and copy the files
-   */
   const servers = await ns.getPurchasedServers();
-
   for (let index = 0; index < servers.length; index++) {
     const targetServer = servers[index];
 
-    /**
-     * Remove all previous script files
-     */
     const serverFiles = ns.ls(targetServer, "js");
     for (const serverFile in serverFiles) {
       await ns.killall(targetServer);
       ns.rm(serverFiles[serverFile], targetServer);
     }
 
-    /**
-     * Copy hack-server file to server if not present and execute it
-     */
     await ns.scp(
       [hackScriptPath, growScriptPath, weakenScriptPath],
       targetServer
     );
   }
 
-  /**
-   * Start the hub so all the servers start working
-   */
   if ((ns.args[0] as string) === "batch") {
     if (!ns.isRunning("/scripts/core/batch-hackingjs", "home")) {
       await ns.run("/scripts/core/batch-hacking.js");
