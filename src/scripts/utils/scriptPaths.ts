b80eea02a77c95540db2f7bd749b064d3cdb6ea4
IAmNotABear <floyd.van.boksel@gmail.com>
2022-11-27 00:35:41 +0100

    Rename hack folder to serverScripts

--- src/scripts/utils/scriptPaths.ts
+++ src/scripts/utils/scriptPaths.ts
@@ -1,3 +1,3 @@
-export const growScriptPath = "/scripts/hacks/grow.js";
-export const weakenScriptPath = "/scripts/hacks/weaken.js";
-export const hackScriptPath = "/scripts/hacks/hack.js";
+export const growScriptPath = "/scripts/serverScripts/grow.js";
+export const weakenScriptPath = "/scripts/serverScripts/weaken.js";
+export const hackScriptPath = "/scripts/serverScripts/hack.js";
