1067efbf54505ad19b37883568f2e119a775f7d1
IAmNotABear <floyd.van.boksel@gmail.com>
2022-11-29 19:55:35 +0100

    Remove redundant comments since they are moved to clickup

--- src/scripts/hacking/batch-hacking.ts
+++ src/scripts/hacking/batch-hacking.ts
@@ -1,29 +1,3 @@
-/**
- * Notes to self:
- *
- * - Need to handle which servers are going to be used since I can't use the
- *   bitburner API to see if a script is going to run since I'll be using
- *   setTimeout to delay triggering the scripts.
- * - Need to see if its needed to create a method to check if a server can
- *   handle the requests (grow and hack in particular) in one go to avoid
- *   requests failing due to security being to high since multiple requests
- *   might have finished before it.
- * - Need to check if servers arn't going to be flooded with multiple small
- *   requests (weaken, grow, hack) which avoid the server ever being able to
- *   handle larger requests (hack and grow in particular).
- * - Check if its worth the time to create a method to see how much ram is
- *   needed if we want to complete requests in one go and mark these servers
- *   so that we will always have some avaible.
- * - Need to check how far ahead we need to batch. Can we manage with just
- *   in time batching where we only ensure the next even to be scheduled or do
- *   we need to complete entire batches and queue the next one as well?
- * - Do we infinitly batch on server or do we split our threads among several
- *   servers?
- * - Check when its realistic to use batch scripting and make a script that
- *   switches between the current hub which forces it way through and the
- *   new batching script.
- */
-
 import getServers from "/scripts/utils/getServers.js";
 import {
   growScriptPath,
@@ -123,7 +97,6 @@ const weakenServer = (ns: NS, servers: string[], event: BatchEvent) => {
     event,
     weakenScriptPath,
     calculateThreadsNeededToWeaken,
-    // event.timeScriptsDone - Date.now() - serverWeakenTime + short,
     event.timeScriptsDone - Date.now() + short,
     {
       status: event.status === "fullyGrown" ? "hackable" : "needsGrowing",
@@ -143,7 +116,6 @@ const growServer = (ns: NS, servers: string[], event: BatchEvent) => {
     event,
     growScriptPath,
     threadsNeededToGrow,
-    // event.timeScriptsDone - Date.now() - growthTime + short,
     event.timeScriptsDone - Date.now() + short,
     {
       status: "fullyGrown",
@@ -318,37 +290,8 @@ const triggerAllServers = async (ns: NS, servers: string[]) => {
   return ns.sleep(short);
 };
 
-/**
- * Batch hack a server enabling maximum profits.
- *
- * The initial implementation will focus on maximizing profits from a single
- * source of income. This is due to our current limitation in ram which will be
- * needed.
- *
- * A single batch consists of four actions:
- * - A hack script removes a predefined, precalculated amount of money from the
- *   target server.
- * - A weaken script counters the security increase of the hack script.
- * - A grow script counters the money decrease caused by the hack script.
- * - A weaken script counters the security increase caused by the grow script.
- *
- * Later versions will enable multiple source of income points to be hacked
- * (and maybe even calculate how many it can handle itself and scale accordingly).
- *
- * @param ns The bitburner NS scope
- */
 export async function main(ns: NS): Promise<void> {
   ns.disableLog("ALL");
-  /**
-   * Before batches can be run a server should always be at minimum security
-   * level to simplify this process
-   *
-   * Steps:
-   * - Grow server to max money
-   * - Weaken server till minimum security
-   * - Hack for max profit
-   * - Weaken server till minimum security
-   */
 
   while (true) {
     let servers = await getServers(ns, {
