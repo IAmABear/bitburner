670ea234fb9712ce68b422889b0f640216448902
IAmNotABear <floyd.van.boksel@gmail.com>
2022-11-30 15:16:19 +0100

    Change buy-server to use a dynamic sleep to speed up initial buying

--- src/scripts/servers/buy-server.ts
+++ src/scripts/servers/buy-server.ts
@@ -3,22 +3,26 @@ import {
   hackScriptPath,
   weakenScriptPath,
 } from "/scripts/utils/scriptPaths";
-import { medium } from "/scripts/utils/timeoutTimes";
+import { long, short } from "/scripts/utils/timeoutTimes";
 
+let dynamicSleep = long;
 export async function main(ns: NS): Promise<void> {
   const ram = Number(ns.args[0]) || 8;
   const i = await ns.getPurchasedServers().length;
 
   while ((await ns.getPurchasedServerLimit()) >= i) {
     if (ns.getPurchasedServerCost(ram) < ns.getServerMoneyAvailable("home")) {
+      dynamicSleep = short;
       const targetServer = await ns.purchaseServer("ghost-" + ram, ram);
 
       await ns.scp(
         [hackScriptPath, growScriptPath, weakenScriptPath],
         targetServer
       );
+    } else {
+      dynamicSleep = long;
     }
 
-    await ns.sleep(medium);
+    await ns.sleep(dynamicSleep);
   }
 }
