e1f7dfb162a5bcbf4fc5d601929f4eca1f5bb4c8
IAmNotABear <floyd.van.boksel@gmail.com>
2022-12-08 13:52:02 +0100

    fixup! Add support to serverScript to handle sleeps

--- src/scripts/serverScripts/weaken.ts
+++ src/scripts/serverScripts/weaken.ts
@@ -1,5 +1,5 @@
 export async function main(ns: NS): Promise<number> {
-  if (ns.args[1] as number) {
+  if ((ns.args[1] as number) > 0) {
     await ns.sleep(ns.args[1] as number);
     return await ns.weaken(ns.args[0] as string);
   } else {
