3d6e87aa8402e2e4e6d0e461ad81c35f667011d0
IAmNotABear <floyd.van.boksel@gmail.com>
2023-01-16 12:35:31 +0100

    Fix bug where only the hack script would be triggered

--- src/scripts/hacking/event-based/v2.ts
+++ src/scripts/hacking/event-based/v2.ts
@@ -1,6 +1,10 @@
 import QueueManger, { QueueEvent } from "/scripts/utils/queueManager";
-import { hackScriptPath } from "/scripts/utils/scriptPaths";
-import { skip } from "/scripts/utils/timeoutTimes";
+import {
+  growScriptPath,
+  hackScriptPath,
+  weakenScriptPath,
+} from "/scripts/utils/scriptPaths";
+import { medium, skip } from "/scripts/utils/timeoutTimes";
 import getWorkerServers from "/scripts/utils/getWorkerServers";
 import runScript, { BatchStatus } from "/scripts/utils/runScript";
 import prepServer from "/scripts/utils/prepServer";
@@ -8,24 +12,28 @@ import prepServer from "/scripts/utils/prepServer";
 const getCompletionStatus = (
   ns: NS,
   event: QueueEvent
-): { status: BatchStatus; scriptCompletionTime: number } => {
+): { status: BatchStatus; scriptCompletionTime: number; script: string } => {
+  ns.print(event);
   switch (event.status) {
     case "hackable":
       return {
         status: "fullyHacked",
         scriptCompletionTime: Math.ceil(ns.getHackTime(event.server)),
+        script: hackScriptPath,
       };
       break;
     case "needsGrowing":
       return {
         status: "fullyGrown",
         scriptCompletionTime: Math.ceil(ns.getGrowTime(event.server)),
+        script: growScriptPath,
       };
       break;
     default:
       return {
         status: event.status === "fullyGrown" ? "hackable" : "needsGrowing",
         scriptCompletionTime: Math.ceil(ns.getWeakenTime(event.server)),
+        script: weakenScriptPath,
       };
       break;
   }
@@ -62,28 +70,30 @@ export async function main(ns: NS): Promise<void> {
     });
     if (events.length === 0) {
       void prepServer(ns, targetServer, workerServers, queueManager);
-    }
+      await ns.sleep(medium);
+    } else {
+      for (let index = 0; index < events.length; index++) {
+        const event = events[index];
+        const completionStatus = getCompletionStatus(ns, event);
 
-    for (let index = 0; index < events.length; index++) {
-      const event = events[index];
-      const completionStatus = getCompletionStatus(ns, event);
-      const res = runScript(
-        ns,
-        workerServers,
-        event,
-        hackScriptPath,
-        event.timeScriptsDone -
-          Date.now() -
-          completionStatus.scriptCompletionTime,
-        completionStatus,
-        queueManager
-      );
+        const res = runScript(
+          ns,
+          workerServers,
+          event,
+          completionStatus.script,
+          event.timeScriptsDone -
+            Date.now() -
+            completionStatus.scriptCompletionTime,
+          completionStatus,
+          queueManager
+        );
 
-      if (!res) {
-        break;
+        if (!res) {
+          break;
+        }
+
+        index++;
       }
-
-      index++;
     }
 
     await ns.sleep(skip);
