804f3cb7d67ab4c47260a73560b4826ba7c85abe
IAmNotABear <floyd.van.boksel@gmail.com>
2023-01-18 10:49:31 +0100

    Add preparingToUpgrade script and copy it to all the servers

---
+++ src/scripts/serverScripts/preparingToUpgrade.ts
@@ -0,0 +1,15 @@
+import { long } from "/scripts/utils/timeoutTimes";
+
+/**
+ * The sole purpose for this script is to run indefinetly so we know the server
+ * has been targeted for an upgrade. This is done to avoid killing servers which
+ * still have scripts triggered on them which could (and most probally will)
+ * screw up timings that are required in event/batch hacking.
+ *
+ * @param ns The NetScript object
+ */
+export async function main(ns: NS): Promise<number> {
+  while (true) {
+    await ns.sleep(long);
+  }
+}
--- src/scripts/utils/scriptPaths.ts
+++ src/scripts/utils/scriptPaths.ts
@@ -1,3 +1,5 @@
 export const growScriptPath = "/scripts/serverScripts/grow.js";
 export const weakenScriptPath = "/scripts/serverScripts/weaken.js";
 export const hackScriptPath = "/scripts/serverScripts/hack.js";
+export const preparingToUpgradeScriptPath =
+  "/scripts/serverScripts/preparingToUpgrade.js";
