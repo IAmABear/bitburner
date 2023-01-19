5f3c703880aed2af084b10617fd032eb22d5cdeb
IAmNotABear <floyd.van.boksel@gmail.com>
2023-01-19 11:37:38 +0100

    Add event-hacking v2 back as the fallback instead of the hub

--- src/scripts/hacking/event-hacking.ts
+++ src/scripts/hacking/event-hacking.ts
@@ -1,6 +1,6 @@
 import { long } from "/scripts/utils/timeoutTimes";
 
-const hubHacking = "/scripts/hacking/hub.js";
+const eventHackingV1 = "/scripts/hacking/event-based/v1.js";
 const eventHackingV2 = "/scripts/hacking/event-based/v2.js";
 
 export async function main(ns: NS): Promise<void> {
@@ -10,8 +10,8 @@ export async function main(ns: NS): Promise<void> {
       (ns.getHackingLevel() >= 20 && ns.getPurchasedServers().length >= 20)
     ) {
       if (!ns.isRunning(eventHackingV2, "home")) {
-        if (ns.isRunning(hubHacking, "home")) {
-          ns.kill(hubHacking, "home");
+        if (ns.isRunning(eventHackingV1, "home")) {
+          ns.kill(eventHackingV1, "home");
         }
 
         ns.exec(eventHackingV2, "home", undefined, ...ns.args);
@@ -19,8 +19,8 @@ export async function main(ns: NS): Promise<void> {
         break;
       }
     } else {
-      if (!ns.isRunning(hubHacking, "home")) {
-        ns.exec(hubHacking, "home", undefined, ...ns.args);
+      if (!ns.isRunning(eventHackingV1, "home")) {
+        ns.exec(eventHackingV1, "home", undefined, ...ns.args);
       }
     }
 
