d4f6fd34f54467f3e9be7d629f51b7bb5e7eddbc
IAmNotABear <floyd.van.boksel@gmail.com>
2023-01-19 11:50:07 +0100

    Fix event-hacking script running checks

--- src/scripts/hacking/event-hacking.ts
+++ src/scripts/hacking/event-hacking.ts
@@ -10,8 +10,8 @@ export async function main(ns: NS): Promise<void> {
       ns.getServer("home").maxRam >= 3200 &&
       ns.getPurchasedServers().length >= 20
     ) {
-      if (!ns.isRunning(eventHackingV2, "home")) {
-        if (ns.isRunning(eventHackingV1, "home")) {
+      if (!ns.isRunning(eventHackingV2, "home", ...ns.args)) {
+        if (ns.isRunning(eventHackingV1, "home", ...ns.args)) {
           ns.kill(eventHackingV1, "home");
         }
 
@@ -20,7 +20,7 @@ export async function main(ns: NS): Promise<void> {
         break;
       }
     } else {
-      if (!ns.isRunning(eventHackingV1, "home")) {
+      if (!ns.isRunning(eventHackingV1, "home", ...ns.args)) {
         ns.exec(eventHackingV1, "home", undefined, ...ns.args);
       }
     }
